0488f718228c549a70820d3a6954c8cd05177d80
wkdwhddn06 <wkdwhddn06@naver.com>
2020-08-07 13:50:16 +0900

    init

---
+++ app/src/main/java/com/clean/citest/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.clean.citest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.text = BuildConfig.DATA
+    }
+}
